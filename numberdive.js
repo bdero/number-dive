b071098405b656cc79a648f03e3f2e57879f9c6a
Brandon DeRosier <x@bdero.me>
2015-11-23 18:39:13 -0500

    Add centered root node

--- numberdive.js
+++ numberdive.js
@@ -1,9 +1,16 @@
 (function() {
 
+  var root;
+
   var resetStageSize = function() {
     var canvas = $("#cjs-canvas");
-    canvas.attr("width", canvas.width());
-    canvas.attr("height", canvas.height());
+    var width = canvas.width();
+    var height = canvas.height();
+    canvas.attr("width", width);
+    canvas.attr("height", height);
+
+    root.x = width/2;
+    root.y = height/2;
   };
 
   var init = function() {
@@ -14,6 +21,10 @@
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
 
+    // Create the root container of the scene graph
+    root = new createjs.Container();
+    stage.addChild(root);
+
     // Add handler for stage resizing
     $(window).resize(resetStageSize);
     resetStageSize();
@@ -22,7 +33,7 @@
     shape.graphics
       .beginFill("lightblue")
       .drawCircle(0, 0, 50);
-    stage.addChild(shape);
+    root.addChild(shape);
   };
 
   // Initialize
